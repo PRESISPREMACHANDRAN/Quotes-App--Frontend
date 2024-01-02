60b971c92c120dd998123e003d1d7764e18ffe48
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-01-02 16:54:06 +0530

    label name changed

--- src/Components/AddQuotes.jsx
+++ src/Components/AddQuotes.jsx
@@ -25,7 +25,7 @@ const AddQuotes = () => {
             <div className="row g-3">
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">
-                  Quote
+                  Give a Quote
                 </label>
                 <input
                   type="text"
@@ -37,7 +37,7 @@ const AddQuotes = () => {
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">
-                  Author
+                  Author Name
                 </label>
                 <input
                   type="text"
